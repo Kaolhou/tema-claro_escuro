ee9e76bfa425aa2129f05187f77d0623ec35c6a8
Jerônimo Mendes <xdroid42@gmail.com>
2021-07-20 14:53:04 -0300

    comentado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function estilos(){
+    //alteração de temas claros para os escuros
     var body = document.body;
     body.classList.toggle("dark-mode");
     document.getElementById("container").classList.toggle("container-dark");
@@ -10,6 +11,7 @@ function teste() {
 }
 
 function readCookie(nome){
+    //função que recebe nome do cookie como parametro e retorna o valor dele
     var vnome = nome + "=";
     var ca = document.cookie.split(';');
     for(var i=0;i<ca.length;i++){
@@ -23,17 +25,17 @@ function readCookie(nome){
 }
 
 function criaCookie(nome, valor, expira){
-    
+    //função de criar cookie com base nos valores de parâmetros
     document.cookie = nome + "=" + valor + "; " + "expires= "+ expira;
-
 }
 function iniCookie(){
+    //criar cookie para tema escuro caso não haja um
     const exist = document.cookie
     if(exist == null || exist == "") return criaCookie("tema-escuro", "true", "Tue, 01 Jan 2115 12:00:00 UTC");
     
     var cookie = readCookie("tema-escuro");
     if(cookie == null || cookie == "") return criaCookie("tema-escuro", "true", "Tue, 01 Jan 2115 12:00:00 UTC");
-    
+    //verificar se o tema está escuro ou claro e altera-lo
     if(cookie == "true"){
         console.log(cookie)
         return document.cookie = "tema-escuro" + "=" + "false" + "; " + "expires= "+ "Tue, 01 Jan 2115 12:00:00 UTC";
@@ -44,6 +46,7 @@ function iniCookie(){
     }
 }
 function carregar(){
+    //ao carregar a página é lodo no cookie seu valor e já aplica tema escuro se necessário
     var cookie = readCookie("tema-escuro");
     if(cookie == "true"){
         estilos()
